fd4be9b2297aa666e3ef4b31695e6681041d76c5
Your Name <irshalfir@gmail.com>
2021-07-24 18:27:47 +0700

    add simple text to activity layout

--- app/src/main/java/com/plygrnd/andr/cmpse/basic/MainActivity.kt
+++ app/src/main/java/com/plygrnd/andr/cmpse/basic/MainActivity.kt
@@ -3,36 +3,41 @@ package com.plygrnd.andr.cmpse.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.plygrnd.andr.cmpse.basic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    JetpackComposeBasicTheme {
+        Surface(color = Color.Yellow) {
+            Greeting(name = "Android")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
