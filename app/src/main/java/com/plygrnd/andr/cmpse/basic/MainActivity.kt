5d8994dca4f14f192856c78f1565a01b278f29d2
Your Name <irshalfir@gmail.com>
2021-07-29 21:42:52 +0700

    animate list click reaction

--- app/src/main/java/com/plygrnd/andr/cmpse/basic/MainActivity.kt
+++ app/src/main/java/com/plygrnd/andr/cmpse/basic/MainActivity.kt
@@ -3,15 +3,16 @@ package com.plygrnd.andr.cmpse.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -68,7 +69,15 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(16.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Composable
