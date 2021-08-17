6e1ae7261884c8ab633b1db3d15f0949cdc842d1
Your Name <irshalfir@gmail.com>
2021-08-17 16:56:42 +0700

    [compose-layout] add modifier example and material design

--- JetpackComposeLayout/app/src/main/java/com/plygrnd/andr/cmpse/jetpackcomposelayout/MainActivity.kt
+++ JetpackComposeLayout/app/src/main/java/com/plygrnd/andr/cmpse/jetpackcomposelayout/MainActivity.kt
@@ -3,13 +3,23 @@ package com.plygrnd.andr.cmpse.jetpackcomposelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddToPhotos
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.plygrnd.andr.cmpse.jetpackcomposelayout.ui.theme.JetpackComposeLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,90 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                LayoutsCodelab()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+/**
+ * Material Components sample
+ */
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeLayoutTheme {
-        Greeting("Android")
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar (
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
     }
 }
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    JetpackComposeLayoutTheme {
+        LayoutsCodelab()
+    }
+}
+
+/**
+ * Modifiers sample
+ */
+
+//@Composable
+//fun PhotographerCard(modifier: Modifier = Modifier) {
+//    Row(modifier
+//        .padding(8.dp)
+//        .clip(RoundedCornerShape(4.dp))
+//        .background(MaterialTheme.colors.surface)
+//        .clickable { /* Ignoring onClick */ }
+//        .padding(16.dp)
+//    ) {
+//        Surface(
+//            modifier = Modifier.size(50.dp),
+//            shape = CircleShape,
+//            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+//        ) {
+//
+//        }
+//        Column(
+//            modifier = Modifier
+//                .padding(start = 8.dp)
+//                .align(Alignment.CenterVertically)
+//        ) {
+//            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+//            // LocalContentAlpha is defining opacity level of its children
+//            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+//                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun PhotographerCardPreview() {
+//    JetpackComposeLayoutTheme {
+//        PhotographerCard()
+//    }
+//}
