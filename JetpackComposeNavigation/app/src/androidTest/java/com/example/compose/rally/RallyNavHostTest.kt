ffb3349f0da216aaa9b6957a841887cb6c35df7b
Uberando MacBook Pro <uberando@macbook.pro>
2022-02-27 16:52:34 +0700

    [compose-navigation] add navigation UI test

--- JetpackComposeNavigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ JetpackComposeNavigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -3,8 +3,14 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Rule
@@ -29,6 +35,50 @@ class RallyNavHostTest {
   fun rallyNavHost() {
     composableTestRule
       .onNodeWithContentDescription("Overview Screen")
+      .assertIsDisplayed()
+  }
+  
+  @Test
+  fun rallyNavHost_navigateToAllAccounts_viaUI() {
+    composableTestRule
+      .onNodeWithContentDescription("All Accounts")
+      .performClick()
+    composableTestRule
+      .onNodeWithContentDescription("Accounts Screen")
+      .assertIsDisplayed()
+  }
+  
+  /**
+   * Use the [navController] to check assertions
+   */
+  @Test
+  fun rallyNavHost_navigateToBills_viaUI() {
+    // When click on "All Bills"
+    composableTestRule
+      .onNodeWithContentDescription("All Bills").apply {
+        performScrollTo()
+        performClick()
+      }
+    // Then the route is "Bills"
+    val route = navController.currentBackStackEntry?.destination?.route
+    assertEquals(route, "Bills")
+  }
+  
+  /**
+   * Navigate using [navController.navigate] and then assert that
+   * the expected screen is displayed
+   */
+  @Test
+  fun rallyNavHost_navigateToAllAccounts_callingNavigate() {
+    // Navigation using [navController.navigate] need to be made on UI thread.
+    // To achieve this, we are using coroutines on the [Main] thread dispatcher
+    runBlocking {
+      withContext(Dispatchers.Main) {
+        navController.navigate(RallyScreen.Accounts.name)
+      }
+    }
+    composableTestRule
+      .onNodeWithContentDescription("Accounts Screen")
       .assertIsDisplayed()
   }
   
